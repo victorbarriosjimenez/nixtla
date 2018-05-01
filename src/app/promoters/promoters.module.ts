09b992fd185f488d5eb9f9092899eab592293590
vicstaples <victorhugo.barrios.jimenez@alumnos.upm.es>
2018-05-01 18:19:38 -0500

    Adding promoters

---
+++ src/app/promoters/promoters.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms'; 
+@NgModule({
+  imports: [
+    CommonModule, 
+    FormsModule,
+    ReactiveFormsModule
+   ],
+  declarations: [
+    
+  ],
+  exports: [
+  ],
+  providers: [ 
+    
+  ]
+})
+export class PromothersModule { }
+ 
