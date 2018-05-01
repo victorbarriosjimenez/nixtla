d09de420c3f048117b45ba86a5341c99eafc25b1
vicstaples <victorhugo.barrios.jimenez@alumnos.upm.es>
2018-05-01 18:23:00 -0500

    Setting up routes and modules

---
+++ src/app/supervisors/supervisors.routes.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes , CanActivate } from '@angular/router';
+const appRoutes: Routes = [  
+  //{ path: , component:  },
+];
+@NgModule({
+  imports: [
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class SupervisorsRoutesModule { }
---
+++ src/app/supervisors/supervisors.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+import {  Administrator, Promoter , Supervisor } from '../models/user';
+@Injectable()
+export class SupervisorsService {
+  admin: Observable<Administrator>;
+  constructor(private afAuth: AngularFireAuth,
+              private afs: AngularFirestore,
+              private router: Router) {
+  }
+}
