3896de1350f542fa4b20897ec09f07654eb9cb8f
vicstaples <victorhugo.barrios.jimenez@alumnos.upm.es>
2018-05-01 01:08:33 -0500

    Creating the app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { HomeComponent } from './auth/home/home.component';
-const appRoutes: Routes = [
-  { path: '', component: RegisterComponent },
+const appRoutes: Routes = [  
+  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 ];
