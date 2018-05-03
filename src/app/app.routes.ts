b7552bb2d0f6699c92678ea1a856d307cf944a70
vicstaples <victorhugo.barrios.jimenez@alumnos.upm.es>
2018-05-03 09:58:12 -0500

    Adding routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,10 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { HomeComponent } from './auth/home/home.component';
 import { AuthGuard } from './auth/authentication.guard';
+import { SettingsComponent } from './auth/settings/settings.component';
 const appRoutes: Routes = [  
   { path: '', component: HomeComponent, canActivate: [AuthGuard], data: { animation: 'home' } },
+  { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard], data: { animation: 'settings' } },  
   { path: 'login', component: LoginComponent,  },
   { path: 'register', component: RegisterComponent  },
 ];
