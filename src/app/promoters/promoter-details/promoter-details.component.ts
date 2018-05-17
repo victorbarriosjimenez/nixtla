b48f2ce852d3bc2a5f679983621e4076f9dd7a68
vicstaples <victorhugo.barrios.jimenez@alumnos.upm.es>
2018-05-17 13:16:08 -0500

    Creating form to add events

--- src/app/promoters/promoter-details/promoter-details.component.ts
+++ src/app/promoters/promoter-details/promoter-details.component.ts
@@ -11,20 +11,6 @@ import {
   CalendarWeekViewBeforeRenderEvent,
   CalendarDayViewBeforeRenderEvent
 } from 'angular-calendar';
-const colors: any = {
-  red: {
-    primary: '#ad2121',
-    secondary: '#FAE3E3'
-  },
-  blue: {
-    primary: '#1e90ff',
-    secondary: '#D1E8FF'
-  },
-  yellow: {
-    primary: '#e3bc08',
-    secondary: '#FDF1BA'
-  }
-};
 @Component({
   selector: 'app-promoter-details',
   templateUrl: './promoter-details.component.html',
